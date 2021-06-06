6ba8cc5a58638825b432d9aedcf504dffdfc8a5a
Inverted Silence <32222093+Inversil@users.noreply.github.com>
2021-06-06 23:10:03 +0200

    1.3.3 update

--- scripts/download.js
+++ scripts/download.js
@@ -14,8 +14,8 @@ twttr.events.bind(
     if (anus == 2) {
       setTimeout(function() {
         if (window.location.pathname == "/download" | window.location.pathname == "/download.html") {
-          window.location.href = atob("L2Rvd25sb2Fkcy9saXZlX2VuaGFuY2VtZW50X3N1aXRlXzEuMy4yLnppcA==");
-          console.log(atob("L2Rvd25sb2Fkcy9saXZlX2VuaGFuY2VtZW50X3N1aXRlXzEuMy4yLnppcA=="));
+          window.location.href = atob("aHR0cHM6Ly9naXRodWIuY29tL0xpdmVFbmhhbmNlbWVudFN1aXRlL0xFU2ZvcldpbmRvd3MvcmVsZWFzZXM=");
+          console.log(atob("aHR0cHM6Ly9naXRodWIuY29tL0xpdmVFbmhhbmNlbWVudFN1aXRlL0xFU2ZvcldpbmRvd3MvcmVsZWFzZXM="));
         }
         if (window.location.pathname == "/download-mac" | window.location.pathname == "/download-mac.html") {
           window.location.href = atob("aHR0cHM6Ly9naXRodWIuY29tL0xpdmVFbmhhbmNlbWVudFN1aXRlL0xFU2Zvck1hY09TL3JlbGVhc2VzL2Rvd25sb2FkL3YxLjMuMS1hbHBoYS9MaXZlRW5oYW5jZW1lbnRTdWl0ZS5kbWc=");
